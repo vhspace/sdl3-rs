48b464cfcc0236ebb30945948ac529f50723a93a
Mischa Spiegelmock <me@mish.dev>
2024-10-14 22:09:54 -0700

    guid haptic hint joystick

--- src/sdl3/hint.rs
+++ src/sdl3/hint.rs
@@ -20,7 +20,7 @@ pub enum Hint {
 ///
 /// # Example
 /// ```rust,no_run
-/// sdl2::hint::set_video_minimize_on_focus_loss(false);
+/// sdl3::hint::set_video_minimize_on_focus_loss(false);
 /// ```
 ///
 /// * `value`: `true` to enable minimizing of the Window if it loses key focus when in fullscreen mode,
@@ -36,7 +36,7 @@ pub fn set_video_minimize_on_focus_loss(value: bool) -> bool {
 ///
 /// # Example
 /// ```rust,no_run
-/// sdl2::hint::set_video_minimize_on_focus_loss_with_priority(false, &sdl2::hint::Hint::Override);
+/// sdl3::hint::set_video_minimize_on_focus_loss_with_priority(false, &sdl3::hint::Hint::Override);
 /// ```
 ///
 /// * `value`: `true` to enable minimizing of the Window if it loses key focus when in fullscreen mode,
@@ -62,10 +62,10 @@ pub fn set_video_minimize_on_focus_loss_with_priority(value: bool, priority: &Hi
 ///
 /// # Example
 /// ```rust,no_run
-/// assert_eq!(sdl2::hint::get_video_minimize_on_focus_loss(), true);
+/// assert_eq!(sdl3::hint::get_video_minimize_on_focus_loss(), true);
 ///
-/// sdl2::hint::set_video_minimize_on_focus_loss(false);
-/// assert_eq!(sdl2::hint::get_video_minimize_on_focus_loss(), false);
+/// sdl3::hint::set_video_minimize_on_focus_loss(false);
+/// assert_eq!(sdl3::hint::get_video_minimize_on_focus_loss(), false);
 /// ```
 pub fn get_video_minimize_on_focus_loss() -> bool {
     match get(VIDEO_MINIMIZE_ON_FOCUS_LOSS) {
@@ -82,10 +82,10 @@ pub fn set(name: &str, value: &str) -> bool {
     let name = CString::new(name).unwrap();
     let value = CString::new(value).unwrap();
     unsafe {
-        sys::SDL_SetHint(
+        sys::hints::SDL_SetHint(
             name.as_ptr() as *const c_char,
             value.as_ptr() as *const c_char,
-        ) == sys::SDL_bool::SDL_TRUE
+        ) == true
     }
 }
 
@@ -96,7 +96,7 @@ pub fn get(name: &str) -> Option<String> {
     let name = CString::new(name).unwrap();
 
     unsafe {
-        let res = sys::SDL_GetHint(name.as_ptr() as *const c_char);
+        let res = sys::hints::SDL_GetHint(name.as_ptr() as *const c_char);
 
         if res.is_null() {
             None
@@ -116,16 +116,16 @@ pub fn set_with_priority(name: &str, value: &str, priority: &Hint) -> bool {
     let value = CString::new(value).unwrap();
 
     let priority_val = match *priority {
-        Hint::Normal => sys::SDL_HintPriority::SDL_HINT_NORMAL,
-        Hint::Override => sys::SDL_HintPriority::SDL_HINT_OVERRIDE,
-        Hint::Default => sys::SDL_HintPriority::SDL_HINT_DEFAULT,
+        Hint::Normal => sys::hints::SDL_HintPriority::SDL_HINT_NORMAL,
+        Hint::Override => sys::hints::SDL_HintPriority::SDL_HINT_OVERRIDE,
+        Hint::Default => sys::hints::SDL_HintPriority::SDL_HINT_DEFAULT,
     };
 
     unsafe {
-        sys::SDL_SetHintWithPriority(
+        sys::hints::SDL_SetHintWithPriority(
             name.as_ptr() as *const c_char,
             value.as_ptr() as *const c_char,
             priority_val,
-        ) == sys::SDL_bool::SDL_TRUE
+        ) == true
     }
 }
