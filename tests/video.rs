4a1c30b3b78cca1a4f034f4f6a89a34e5001ecf3
Naum Azeredo <naumazeredo@gmail.com>
2025-07-04 18:57:54 -0700

    Make VideoSubsystem::window_from_id unsafe (#191)

--- tests/video.rs
+++ tests/video.rs
@@ -17,3 +17,38 @@ fn display_name_no_segfault() {
       // so ignore it
 }
 */
+
+use sdl3::render::Canvas;
+use sdl3::video::Window;
+use sdl3::VideoSubsystem;
+
+fn build_canvas(video_subsystem: VideoSubsystem) -> Canvas<Window> {
+    let window = video_subsystem
+        .window("rust-sdl3 test: Video", 800, 600)
+        .resizable()
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())
+        .unwrap();
+
+    let mut canvas = window.into_canvas();
+
+    canvas.clear();
+    canvas
+}
+
+#[test]
+fn window_from_id() {
+    let sdl_context = sdl3::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let canvas = build_canvas(video_subsystem.clone());
+
+    let mut window = unsafe {
+        video_subsystem
+            .window_from_id(canvas.window().id())
+            .unwrap()
+    };
+    window.maximize();
+}
